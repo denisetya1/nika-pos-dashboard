36bb2136162ffd8a256d530c79419e4188144668
Deni <deni.setyawan@tiket.com>
2024-01-19 23:47:42 +0700

    test print

--- src/app/pos/components/ComponentToPrint.tsx
+++ src/app/pos/components/ComponentToPrint.tsx
@@ -4,7 +4,33 @@ import React from 'react'
 
 class ComponentToPrint extends React.Component {
   render (){
-    return <div>ComponentToPrint</div>
+    return (<div>
+      <div className="logo flex justify-center py-5">
+        <img
+                  alt="NIKA POS - BEAUTYCAT"
+                  height="24"
+                  src="/receipt-logo.png"
+                  width="150"
+                />
+      </div>
+      <div className='border-y-[2px] p-3'>
+        1234567
+      </div>
+      <div className='p-3'>
+        <div className='flex justify-between align-top'>
+          <div>1 Testing produk @20000</div><div>20000</div>
+        </div>
+        <div className='flex justify-between align-top'>
+          <div>2 Testing produk A @25000</div><div>50000</div>
+        </div>
+        <div className='flex justify-between align-top'>
+          <div>1 Testing produk C @10000</div><div>10000</div>
+        </div>
+      </div>
+      <div className='border-y-[2px] p-3'>
+        Total
+      </div>
+    </div>)
   }
 }
 
--- src/app/pos/page.tsx
+++ src/app/pos/page.tsx
@@ -16,20 +16,21 @@ const POSPage = () => {
     <div className="w-full h-screen flex justify-start align-top">
       <div className="w-[400px] h-screen bg-red-300 p-8">
         <div className="receipt-logo">
-          <Image
+          {/* <Image
                   alt="NIKA POS - BEAUTYCAT"
                   height="24"
                   src="/logo-stroked.png"
                   width="150"
-                />
+                /> */}
+          
+          <div>
+            <ComponentToPrint ref={componentRef} />
+            <button onClick={handlePrint}>Print this out!</button>
+          </div>
         </div>
       </div>
       <div className="grow h-screen bg-blue-300 p-10">
         dsa
-      </div>
-      <div>
-        <ComponentToPrint ref={componentRef} />
-        <button onClick={handlePrint}>Print this out!</button>
       </div>
     </div>
   )
