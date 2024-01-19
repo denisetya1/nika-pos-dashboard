9bc8411168e244729d3c9d58dfbd92fe58d46173
Deni <deni.setyawan@tiket.com>
2024-01-19 23:48:12 +0700

    test print

--- src/app/pos/page.tsx
+++ src/app/pos/page.tsx
@@ -14,7 +14,7 @@ const POSPage = () => {
 
   return (
     <div className="w-full h-screen flex justify-start align-top">
-      <div className="w-[400px] h-screen bg-red-300 p-8">
+      <div className="w-[400px] h-screen p-8">
         <div className="receipt-logo">
           {/* <Image
                   alt="NIKA POS - BEAUTYCAT"
