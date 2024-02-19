5bedf05957187ee7b95744fd0a00a9d9abefd588
Deni <deni.setyawan@tiket.com>
2024-02-19 14:26:49 +0700

    add new api

---
+++ src/app/api/pos/[outletId]/shifts/close/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../client";
+import { verifyJwt } from "@/app/lib/jwt";
+import moment from "moment";
+
+export const POST = async (req: NextRequest, {params} : { params: {
+  outletId: string
+}}) =>  {
+  const authorization = req.headers.get('authorization') || ''
+  const [__, accessToken] = authorization.split(' ')
+
+  const userData = verifyJwt(accessToken)
+
+  if(accessToken && userData) {
+    const body = await req.json()
+    const {shiftId, capital, userId} = body
+
+    let userShift = await prisma.userShift.findFirst({
+      where:{
+        shiftId: Number(shiftId),
+        userId,
+        startDateTime: {
+          gte: new Date(moment().format("yyyy-MM-dd")),
+        }
+      }
+    })
+
+    if(!userShift) {
+      userShift = await prisma.userShift.create({
+        data: {
+          shiftId: Number(shiftId),
+          userId,
+          capital: Number(capital),
+          startDateTime: new Date()
+        }
+      })
+    }
+
+    return NextResponse.json(userShift);
+
+  } else {
+    return NextResponse.json({
+      code: "UNATHORIZED",
+      message: "unathorized"
+    }, {
+      status: 401
+    });
+  }
+}
