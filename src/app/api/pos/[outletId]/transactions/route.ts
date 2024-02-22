25877186f8d6993f0efe1d9e375300ed8bda77c2
Deni <deni.setyawan@tiket.com>
2024-02-22 13:11:59 +0700

    add save transaction

---
+++ src/app/api/pos/[outletId]/transactions/route.ts
@@ -0,0 +1,71 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../client";
+import { verifyJwt } from "@/app/lib/jwt";
+
+export const POST = async (req: NextRequest) =>  {
+  const authorization = req.headers.get('authorization') || ''
+  const [__, accessToken] = authorization.split(' ')
+
+  const userData = verifyJwt(accessToken)
+
+  if(accessToken && userData) {
+    const body = await req.json()
+
+    const transaction = await prisma.transaction.create({
+      data: {
+        id: body.id,
+        outletId: body.outletId,
+        storeId: body.storeId,
+        userId: body.userId,
+        userShiftId: body.userShiftId,
+
+        totalItem: body.totalItem,
+        totalPrice: body.totalPrice,
+        totalDiscount: 0,
+        amountPaid: body.amountPaid,
+        amountChange: body.amountChange,
+
+        outletPaymentMethodId: body.outletPaymentMethodId,
+        cardNumber: body.cardNumber,
+        confirmNumber: body.confirmNumber,
+        transactionTime: body.transactionTime,
+        transactionDetails: {
+          createMany: {
+            data: body.transactionDetails
+          }
+        }
+      }, 
+      include:{
+        transactionDetails: true
+      }
+    })
+
+    transaction.transactionDetails.map(async (product) => {
+      const ps = await prisma.productStock.update({
+        where: {
+          id: product.productStockId
+        },
+        data: {
+          quantity: {
+            increment: -1*product.qty
+          }
+        }
+      })
+    })
+
+    return NextResponse.json({
+      code: "SUCCESS",
+      message: "",
+      data: transaction
+    });
+
+  } else {
+    return NextResponse.json({
+      code: "UNATHORIZED",
+      message: "unathorized",
+      data: null
+    }, {
+      status: 401
+    });
+  }
+}
