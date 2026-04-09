e18e453ad7be72cdea71dca238e5b9bfdbfbe89c
Deni Setyawan <deni.setyawan@gmail.com>
2026-04-09 14:27:33 +0700

    remove otp

--- src/app/api/pos/[outletId]/transactions/[transactionId]/request-otp/route.ts
+++ src/app/api/pos/[outletId]/transactions/[transactionId]/request-otp/route.ts
@@ -3,84 +3,97 @@ import { prisma } from "@/lib/client";
 import { verifyJwt } from "@/lib/jwt";
 import sendMail from "@/lib/sendMail";
 
-export const POST = async (req: NextRequest, {params} : { params: {
-  transactionId: string
-}}) =>  {
-  const authorization = req.headers.get('authorization') || ''
-  const [__, accessToken] = authorization.split(' ')
+export const POST = async (
+  req: NextRequest,
+  {
+    params,
+  }: {
+    params: {
+      transactionId: string;
+    };
+  },
+) => {
+  const authorization = req.headers.get("authorization") || "";
+  const [__, accessToken] = authorization.split(" ");
 
-  const userData = verifyJwt(accessToken)
+  const userData = verifyJwt(accessToken);
 
-  if(accessToken && userData) {
-    const {
-      transactionId
-    } = params
-    
+  if (accessToken && userData) {
+    const { transactionId } = params;
+
     const transaction = await prisma.transaction.findUnique({
       where: {
-        id: transactionId
-      }
-    }) 
-    
-    if(transaction){
+        id: transactionId,
+      },
+    });
 
-      const code = Math.random().toString().substring(2, 8)
+    if (transaction) {
+      const code = Math.random().toString().substring(2, 8);
 
       const request = await prisma.requestEditTransaction.create({
         data: {
           transactionId,
-          code
-        }
-      })
+          code,
+        },
+      });
 
       const htmlContent = `<p>${userData.name} meminta untuk mengubah transaksi <strong>${transaction.id}</strong>.</p>
       <p>berikut Kode OTP untuk melakukan perubahan:<br /><br /><br /></p>
-      <p style="text-align: center"><strong style="font-size: 45px">${code}</strong><br /><br /><br /></p>`
-      
-      await sendMail({
-        sender: {
-          'name': 'No-Reply',
-          'email': 'no-reply@mailer.beautycat.id',
-        },
-        to: [{
-          name: 'Deni',
-          email: 'deni.setyawan@gmail.com'
-        }],
-        subject: `Permintaan Edit Transaksi: ${transaction.id}`,
-        htmlContent
-      })
+      <p style="text-align: center"><strong style="font-size: 45px">${code}</strong><br /><br /><br /></p>`;
 
-      if(request){
+      // await sendMail({
+      //   sender: {
+      //     'name': 'No-Reply',
+      //     'email': 'no-reply@mailer.beautycat.id',
+      //   },
+      //   to: [{
+      //     name: 'Deni',
+      //     email: 'deni.setyawan@gmail.com'
+      //   }],
+      //   subject: `Permintaan Edit Transaksi: ${transaction.id}`,
+      //   htmlContent
+      // })
+
+      if (request) {
         return NextResponse.json({
           code: "SUCCESS",
           message: "",
-          data: request
+          data: request,
         });
       } else {
-        return NextResponse.json({
-          code: "ERROR",
-          message: "Gagal menyimpan data!",
-          data: null
-        }, {
-          status: 400
-        });
+        return NextResponse.json(
+          {
+            code: "ERROR",
+            message: "Gagal menyimpan data!",
+            data: null,
+          },
+          {
+            status: 400,
+          },
+        );
       }
     } else {
-      return NextResponse.json({
-        code: "DATA_NOT_EXISTS",
-        message: "Data tidak ditemukan!",
-        data: null
-      }, {
-        status: 404
-      });
+      return NextResponse.json(
+        {
+          code: "DATA_NOT_EXISTS",
+          message: "Data tidak ditemukan!",
+          data: null,
+        },
+        {
+          status: 404,
+        },
+      );
     }
   } else {
-    return NextResponse.json({
-      code: "UNATHORIZED",
-      message: "Unathorized Error!",
-      data: null
-    }, {
-      status: 401
-    });
+    return NextResponse.json(
+      {
+        code: "UNATHORIZED",
+        message: "Unathorized Error!",
+        data: null,
+      },
+      {
+        status: 401,
+      },
+    );
   }
-}
+};
--- src/app/api/pos/[outletId]/transactions/[transactionId]/verify-otp/route.ts
+++ src/app/api/pos/[outletId]/transactions/[transactionId]/verify-otp/route.ts
@@ -1,66 +1,73 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/client";
 import { verifyJwt } from "@/lib/jwt";
-import sendMail from "@/lib/sendMail";
 
-export const POST = async (req: NextRequest, {params} : { params: {
-  transactionId: string
-}}) =>  {
-  const authorization = req.headers.get('authorization') || ''
-  const [__, accessToken] = authorization.split(' ')
+export const POST = async (
+  req: NextRequest,
+  {
+    params,
+  }: {
+    params: {
+      transactionId: string;
+    };
+  },
+) => {
+  const authorization = req.headers.get("authorization") || "";
+  const [__, accessToken] = authorization.split(" ");
 
-  const userData = verifyJwt(accessToken)
+  const userData = verifyJwt(accessToken);
 
-  if(accessToken && userData) {
-    const body = await req.json()
+  if (accessToken && userData) {
+    const body = await req.json();
 
-    const {
-      transactionId
-    } = params
+    const { transactionId } = params;
 
-    const {
-      code
-    } = body
-    
+    const { code } = body;
+
     const editAuthorized = await prisma.requestEditTransaction.findFirst({
       where: {
         transactionId,
-        code
-      }
-    }) 
-    
-    if(editAuthorized){
+        code,
+      },
+    });
+
+    if (editAuthorized) {
       const transaction = await prisma.transaction.findFirst({
         where: {
-          id: transactionId
+          id: transactionId,
         },
         include: {
-          transactionDetails: true
-        }
-      })
+          transactionDetails: true,
+        },
+      });
 
       return NextResponse.json({
         code: "SUCCESS",
         message: "",
-        data: transaction
+        data: transaction,
       });
-     
     } else {
-      return NextResponse.json({
-        code: "DATA_NOT_EXISTS",
-        message: "OTP tidak valid!",
-        data: null
-      }, {
-        status: 404
-      });
+      return NextResponse.json(
+        {
+          code: "DATA_NOT_EXISTS",
+          message: "OTP tidak valid!",
+          data: null,
+        },
+        {
+          status: 404,
+        },
+      );
     }
   } else {
-    return NextResponse.json({
-      code: "UNATHORIZED",
-      message: "Unathorized Error!",
-      data: null
-    }, {
-      status: 401
-    });
+    return NextResponse.json(
+      {
+        code: "UNATHORIZED",
+        message: "Unathorized Error!",
+        data: null,
+      },
+      {
+        status: 401,
+      },
+    );
   }
-}
+};
