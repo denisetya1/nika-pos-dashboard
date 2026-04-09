c78e93bce4ca0a8349ce48a585172f0ab9ce1400
Deni Setyawan <deni.setyawan@gmail.com>
2026-04-09 13:59:37 +0700

    fix jwt

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,29 +1,30 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 
- 
- interface SignOption {
+interface SignOption {
   expiresIn?: string | number;
- }
+}
 
- const DEFAULT_SIGN_OPTION: SignOption = {
-  expiresIn: "12h"
- }
+const DEFAULT_SIGN_OPTION: SignOption = {
+  expiresIn: "12h",
+};
 
- export const signJwtAccessToken = (payload: JwtPayload, options: SignOption = DEFAULT_SIGN_OPTION) => {
-  const secretKey = process.env.SECRET_KEY
+export const signJwtAccessToken = (payload: JwtPayload) => {
+  const secretKey = process.env.SECRET_KEY;
 
-  const token = jwt.sign(payload, secretKey as string, options)
+  const token = jwt.sign(payload, secretKey as string, {
+    expiresIn: "12h",
+  });
 
-  return token
- }
+  return token;
+};
 
- export const verifyJwt = (token: string) => {
-  try{
-    const secretKey = process.env.SECRET_KEY
-    const decoded = jwt.verify(token, secretKey as string)
+export const verifyJwt = (token: string) => {
+  try {
+    const secretKey = process.env.SECRET_KEY;
+    const decoded = jwt.verify(token, secretKey as string);
 
-    return decoded as JwtPayload
+    return decoded as JwtPayload;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
- }
+};
